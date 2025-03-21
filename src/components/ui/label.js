24776ad55f8e4d48db32ae728042c6a07c13d9a1
Samantha Jeet <samanthajeet@gmail.com>
2025-03-21 00:35:20 -0600

    Update Label component styles with new color and background options
    - Replaced existing color classes with hex color codes for better customization.
    - Introduced a new `bgcolor` object to manage background color classes for enhanced styling flexibility.

--- src/components/ui/label.js
+++ src/components/ui/label.js
@@ -2,11 +2,17 @@ import { cx } from "@/utils/all";
 
 export default function Label(props) {
     const color = {
-        green: "text-emerald-700",
-        blue: "text-blue-600",
-        orange: "text-orange-700",
-        purple: "text-purple-600",
-        pink: "text-pink-600"
+        "#a3b18a": "text-[#a3b18a]",
+        '#457b9d': "text-[#457b9d]",
+        '#9f86c0': "text-[#9f86c0]",
+        '#ee9b00': "text-[#ee9b00]",
+    };
+    const bgcolor = {
+        green: "bg-emerald-50",
+        blue: "bg-blue-50",
+        orange: "bg-orange-50",
+        purple: "bg-purple-50",
+        pink: "bg-pink-50"
     };
     const margin = props.nomargin;
 
